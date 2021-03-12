3dda7080b08cd71fb0276c1aec3b97138cf7791a
James Heavey <james@heavey.net>
2021-03-12 19:35:46 +0000

    Designed 'PasswordTo7Seg'
    Created a module to convert password
    into seven segment representation.

--- PasswordTo7Seg.v
+++ PasswordTo7Seg.v
@@ -1,48 +1,43 @@
-//// Move all this to different 7seg handler module, chnage thsi module to DigitalLockFSM
-//// Create a top level module that runs both the fsm, the 7 seg and other submodules
-//// make only pos edge button module then use that to connect into the finite state machine buttons
+/*
+ * ELEC5566 Assignment 2:
+ * Password to 7 Segment Conversion Submodule
+ * ---------------------------------
+ * For: University of Leeds
+ * Date: 10/3/2021
+ *
+ * Description
+ * ---------------------------------
+ * Module to convert key press outputs 
+ * from FSM to seven segment displays
+ *
+ */
 
-//PasswordTo7Seg #(
-//
-//	.LENGTH		( length )
-//	
-//) displays (
-//	
-//	.password	( display ),
-//	.SevenSegs	( )
-//
-//);
+module PasswordTo7Seg #(
 
-//integer length;
-//reg [(6*4)-1:0] display;
-//
-//always @(posedge clock) begin
-//
-//	length = key_presses;
-//
-//	case (state)
-//		  
-//		CREATE_PASSWORD: begin 
-//			if (key_presses < PASSWORD_LENGTH) begin
-//				length = key_presses + 1;
-//				display = temp_password >> 4*(PASSWORD_LENGTH - key_presses);
-//			end else begin
-//				length = key_presses - PASSWORD_LENGTH;
-//				display = password >> 4*(key_presses);
-//			end
-//		end
-//	  
-//		ENTER_PASSWORD: begin
-//			length = key_presses + 1;
-//			display = password >> 4*key_presses;
-//		end
-//		  
-//		default: begin
-//			display = {((6*4)-1){1'bx}};
-//		end
-//		  
-//	endcase
-//	
-//end
-//
-//endmodule
+	parameter NUM_DISPLAYS = 6
+
+)(
+
+	input [(4*NUM_DISPLAYS)-1:0] password,
+	
+	output [(7*NUM_DISPLAYS)-1:0] SevenSeg
+	
+);
+
+genvar i;
+
+generate 
+
+	for (i = 0; i < NUM_DISPLAYS; i = i + 1) begin : SevenSeg_loop
+
+		HexTo7Segment display (
+			.hex			( password[(i*4)+:4] ),
+			.SevenSeg	( SevenSeg[(i*7)+:7] )
+		);
+		
+	end 
+
+endgenerate
+
+endmodule
+	
