0a4e4a9a3e28321f20295bafa7c1c38e994f9df4
James Heavey <james@heavey.net>
2021-03-13 18:19:07 +0000

    Edited 'HexTo7Seg'
    Removed all numbers except 1-4, added letter characters for advanced error messages.

--- HexTo7Segment.v
+++ HexTo7Segment.v
@@ -24,21 +24,24 @@ always @(*) begin
 	case(hex)
 	
 		4'h0: SevenSeg = ~7'b0000000;  // no need to represent 0
-		4'h1: SevenSeg = ~7'b0000110;
-		4'h2: SevenSeg = ~7'b1011011;
-		4'h3: SevenSeg = ~7'b1001111;
-		4'h4: SevenSeg = ~7'b1100110;
-		4'h5: SevenSeg = ~7'b1101101;
-		4'h6: SevenSeg = ~7'b1111101;
-		4'h7: SevenSeg = ~7'b0000111;
-		4'h8: SevenSeg = ~7'b1111111;
-		4'h9: SevenSeg = ~7'b1100111;
-		4'hA: SevenSeg = ~7'b1110111;
-		4'hB: SevenSeg = ~7'b1111100;
-		4'hC: SevenSeg = ~7'b0111001;
-		4'hD: SevenSeg = ~7'b1011110;
-		4'hE: SevenSeg = ~7'b1111001;
-		4'hF: SevenSeg = ~7'b1110001;
+		
+		4'h1: SevenSeg = ~7'b0000110; // 1
+		4'h2: SevenSeg = ~7'b1011011; // 2
+		4'h4: SevenSeg = ~7'b1001111; // 3
+		4'h8: SevenSeg = ~7'b1100110; // 4
+		
+		4'h3: SevenSeg = ~7'b1111001; // E
+		4'h5: SevenSeg = ~7'b1010000; // r
+		4'h6: SevenSeg = ~7'b0111111; // O
+		
+		4'h7: SevenSeg = ~7'b0111110; // U
+		4'h9: SevenSeg = ~7'b1010100; // n
+		4'hA: SevenSeg = ~7'b0111000; // L
+		4'hB: SevenSeg = ~7'b0111001; // C
+		4'hC: SevenSeg = ~7'b1011110; // d
+		
+		4'hD: SevenSeg = ~7'b1101101; // S
+		4'hE: SevenSeg = ~7'b1111000; // t
 		
 		default: SevenSeg = 7'b0000000;
 		
